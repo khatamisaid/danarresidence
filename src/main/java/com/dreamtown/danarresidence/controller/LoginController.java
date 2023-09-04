0bfcf0be837dc23742e9ed544ab3b77bca42bf2f
Said Muhammad Khatami <33-said@users.noreply.gitlab.tecmint.com>
2023-09-04 13:58:57 +0700

    update

--- src/main/java/com/dreamtown/danarresidence/controller/LoginController.java
+++ src/main/java/com/dreamtown/danarresidence/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.dreamtown.danarresidence.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,13 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.dreamtown.danarresidence.entity.Website;
+import com.dreamtown.danarresidence.repository.WebsiteRepository;
+
 @Controller
 public class LoginController {
+
+    @Autowired
+    private WebsiteRepository websiteRepository;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model) throws Exception {
+        Website web = websiteRepository.findAll().get(0);
         if (isAuthenticated()) {
             return "redirect:/admin";
         }
+        model.addAttribute("web", web);
         return "auth/login";
     }
 
